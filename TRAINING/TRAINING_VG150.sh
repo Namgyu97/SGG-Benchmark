82a9b4a2d8d9a6a8ba4ac69df2c0d0ea34c0944d
Namgyu97 <jng6017@gachon.ac.kr>
2024-05-14 10:23:21 +0900

    Initialization

--- TRAINING/TRAINING_VG150.sh
+++ TRAINING/TRAINING_VG150.sh
@@ -1,5 +1,8 @@
-### PE-NET
+###
+CUDA_VISIBLE_DEVICES=0 python tools/detector_pretest_net.py --config-file "configs/detector_R_101_FPN_1x.yaml" WEIGHT ./checkpoints/VG150/OBJDET/faster_rcnn/FasterRCNN_Motifs.pth OUTPUT_DIR ./checkpoints/VG150/OBJDET/faster_rcnn
 
-CUDA_VISIBLE_DEVICES=0 python tools/relation_train_net.py --save-best --use-wandb --task sgdet --config-file "configs/VG150/e2e_relation_yolov8m.yaml" MODEL.ROI_RELATION_HEAD.PREDICTOR PrototypeEmbeddingNetwork SOLVER.IMS_PER_BATCH 16 TEST.IMS_PER_BATCH 1 DTYPE "float16" SOLVER.PRE_VAL True GLOVE_DIR /home/maelic/glove OUTPUT_DIR ./checkpoints/VG150/SGDET/penet-yolov8m
-
-CUDA_VISIBLE_DEVICES=0 python tools/relation_train_net.py --save-best --use-wandb --task sgdet --config-file "configs/VG150/demo_motifs_faster_rcnn.yaml" MODEL.ROI_RELATION_HEAD.PREDICTOR PrototypeEmbeddingNetwork SOLVER.IMS_PER_BATCH 16 TEST.IMS_PER_BATCH 1 DTYPE "float16" SOLVER.PRE_VAL True GLOVE_DIR /home/maelic/glove OUTPUT_DIR ./checkpoints/VG150/SGDET/penet-faster_rcnn
+#### PE-NET
+#
+#CUDA_VISIBLE_DEVICES=0 python tools/relation_train_net.py --save-best --use-wandb --task sgdet --config-file "configs/VG150/e2e_relation_yolov8m.yaml" MODEL.ROI_RELATION_HEAD.PREDICTOR PrototypeEmbeddingNetwork SOLVER.IMS_PER_BATCH 16 TEST.IMS_PER_BATCH 1 DTYPE "float16" SOLVER.PRE_VAL True GLOVE_DIR /home/maelic/glove OUTPUT_DIR ./checkpoints/VG150/SGDET/penet-yolov8m
+#
+#CUDA_VISIBLE_DEVICES=0 python tools/relation_train_net.py --save-best --use-wandb --task sgdet --config-file "configs/VG150/demo_motifs_faster_rcnn.yaml" MODEL.ROI_RELATION_HEAD.PREDICTOR PrototypeEmbeddingNetwork SOLVER.IMS_PER_BATCH 16 TEST.IMS_PER_BATCH 1 DTYPE "float16" SOLVER.PRE_VAL True GLOVE_DIR /home/maelic/glove OUTPUT_DIR ./checkpoints/VG150/SGDET/penet-faster_rcnn
